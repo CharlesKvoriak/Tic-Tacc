f857ed5d4290024b9f8ae57ce02422a11ec1a7f0
Charles Kvoriak <s_charles.kvoriak@ousd.org>
2019-12-19 02:06:03 +0000

    It works now

--- script.js
+++ script.js
@@ -2,23 +2,39 @@ var turn = "x";
 var turnsTaken =  0;
 var gameOver = false;
 var getnum = new RegExp("[0-9]");
-var board = [[0, 0, 0],
-             [0, 0, 0],
-             [0, 0, 0]];
-
+var board = [[1, 2, 3],// initialize array with these  
+             [4, 5, 6],//values just to avoid the 
+             [7, 8, 9]];//diagonals being equal before
+                        //play
 function performLogic(buttonId, tileId){
   if(!gameOver){
     $(tileId).html(turn);
     var x = (buttonId.match(getnum)[0]-1)%3;
     var y = Math.floor((buttonId.match(getnum)[0]-1)/3);
     board[x][y] = turn;
-    turn = switchTurn(turn);
 
     turnsTaken++;
     if(turnsTaken === 9){
       gameOver = true;
-      $("h1").text("It's a draw!")
+      $("h1").text("It's a draw!");
     }
+
+    if(board[x][0] === board[x][1] && board[x][1] === board[x][2]){
+      gameOver = true;
+      $("h1").text(`${turn} wins!`);
+    }
+
+    if(board[0][y] === board[1][y] && board[1][y] === board[2][y]){
+      gameOver = true;
+      $("h1").text(`${turn} wins!`);
+    }
+
+    if((board[0][0] === board[1][1] && board[1][1] === board[2][2]) || (board[0][2] === board[1][1] && board[1][1] === board[2][0])){
+      gameOver = true;
+      $("h1").text(`${turn} wins!`);
+    }
+
+    turn = switchTurn(turn);
   }
 }
 
